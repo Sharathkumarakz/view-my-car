65c4b761db46b95b41fcc08a42379c0543b949c0
Sharath Kumar A <sharathkannanakz@gmail.com>
2024-02-28 20:39:19 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'view my car',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
